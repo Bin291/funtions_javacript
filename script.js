f22e09713401513e1c21022f4a497f884e09677f
Binh <phuongbinh200429@gmail.com>
2025-02-13 11:50:38 +0700

    feat: chia gon tung bai

--- script.js
+++ script.js
@@ -8,14 +8,20 @@
 // let result=add();
 // console.log(result);
 
+
+
+
+
 // function add(a,b){
 //     return a+b;
 // }
-
 // // let a = 100;
 // // let b = 15;
 // let result = add(12,24231);
 // console.log(result)
+
+
+
 
 // function minus(a,b){
 //     return a-b;
@@ -23,33 +29,34 @@
 // function minus(a,b){
 //     return b-a;
 // }
-// // let a = 100;
-// // let b = 15;
 // let result = minus(13,13);
 // console.log(result);
 // let result2= minus(12,13);
 // console.log(result2);
 
+
+
+
 // function mutiply(a,b){
 //     return a*b;
 // }
-
 // function devide(a,b){
 //     return a/b;
 // }
-
 // let result = mutiply(7,7);
 // console.log(result);
 // let result2= devide(12,12);
 // console.log(result2);
 
+
+
 // function linearEquationSolve(a,b){
 //     return -b/a;
-
 // }
-
 // let result = linearEquationSolve(0,0);
 // console.log(result)
+
+
 
 // function linearEquationSolve(a,b){
 // if(a!=0 && b!=0){
@@ -57,16 +64,16 @@
 // }
 // if(a!=0 && b==0){
 //     return "Vo nghiem"
-
 // }
 // if(a==0&&b==0) {
 //     return "vo so nghiem"
 // }
-
 // }
-
 // const x = linearEquationSolve(0,0);
 // console.log(x)
+
+
+
 
 // function giaiPhuongTrinhBachai(a,b,c){
 //     let delta = ((b*b)-(4*a*c))
@@ -99,6 +106,11 @@
 // let result = giaiPhuongTrinhBachai(1,-49,-50)
 // console.log(result)
 
+
+
+
+
+
 // TINH NAM NHUAN
 // function tinhLichNhuan(year) {
 //     if (year % 400 == 0) {
@@ -115,19 +127,24 @@
 //  console.log(result)
 
 
+
+
+
+
 //TINH CAN CHI
 // function CanChi(year) {
 //     const canList = ["Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ", "Canh", "Tân", "Nhâm", "Quý"];
 //     const chiList = ["Tý", "Sửu", "Dần", "Mão", "Thìn", "Tỵ", "Ngọ", "Mùi", "Thân", "Dậu", "Tuất", "Hợi"];
-
 //     let can = canList[(year + 6) % 10]; 
 //     let chi = chiList[(year + 8) % 12]; 
-
 //     return `${can} ${chi}`;
 // }
 // let nam = CanChi(2004)
 // console.log(nam)
  
+
+
+
 
 // //Tinh chuoi so lon
 // function add(a,b){
@@ -135,17 +152,14 @@
 //     let d = BigInt(b)
 //     return (c+d).toString()
 // }
-
 // let result = add(1265213626735293612763 , 2187389127362713671217236187213)
 // console.log(result)
-
 
 // function multiply(a,b){
 //     let c = BigInt(a)
 //     let d = BigInt(b)
 //     return (c*d).toString()
 // }
-
 // let result1 = multiply(1265213626735293612763 , 2187389127362713671217236187213)
 // console.log(result1)
 
